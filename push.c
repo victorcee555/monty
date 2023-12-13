5878ae8a008b6643d4bf7d31932586771e6003a7
Victor Efobi <victorefobi1@gmail.com>
2023-12-13 11:43:11 -0800

    push function added

---
+++ push.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+ * push - A function to add ontop of the stack.
+ *
+ * @line_number: The line number from the monty file.
+ *
+ * Return: void.
+ */
+
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_stack;
+
+	new_stack = malloc(sizeof(stack_t));
+	if (new_stack == NULL)
+	{
+		free(new_stack);
+		exit(EXIT_FAILURE);
+	}
+
+	new_stack->n = value;
+	new_stack->next = *stack;
+	new_stack->prev = NULL;
+	if ((*stack) != NULL)
+		(*stack)->prev = new_stack;
+	(*stack) = new_stack;
+
+	printf("%d\n" new_stack->n);
+}
