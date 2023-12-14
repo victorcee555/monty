b4dc96241e01a562d4eef3abc6b9d94d82feb93d
Saviour Assandoh <asandohs@gmail.com>
2023-12-14 13:03:19 -0800

    No operation

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing
+ * @stack: pointer to a pointer to a stack
+ * @line_number: current line number
+ *
+ * Return: nothing
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
